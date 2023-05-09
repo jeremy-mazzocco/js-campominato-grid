bd05c69b817c710487cb44f0a45157a27d718600
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-09 14:41:45 +0100

    add bonus

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-let livelloDiDifficolta = 100;
+// BONUS: Prendi il livello di difficolta dal DOM e settalo in una variabile
+const livelloSelezionato = document.getElementById('difficolta').value;
 
 // Aggiungo funzionalita' al click del Bottone
 const selectButton = document.getElementById('btn');
 selectButton.addEventListener('click',
     function () {
+        let numeroDiCelle;
+        // const numeroDiCelle = 100;
+
+        // BONUS: in base al livello selezionato: assegna il giusto valore alla variabile numeroDiCelle
+        if (livelloSelezionato === "Easy") {
+            numeroDiCelle = 100;
+        } else if (livelloSelezionato === "Medium") {
+            numeroDiCelle = 81;
+        } else if (livelloSelezionato === "Hard") {
+            numeroDiCelle = 49;
+        }
 
         // Cicla per 100 volte le seguenti operazioni
-        for (let i = 1; i < livelloDiDifficolta + 1; i++) {
+        for (let i = 1; i < numeroDiCelle + 1; i++) {
 
             // Crea un elemento
             const createSquare = document.createElement('div');
@@ -21,9 +33,18 @@ selectButton.addEventListener('click',
             // scrivi all'interno dell'elemento
             createSquare.innerHTML += i;
 
+            // BONUS: in base al livello selezionato: cambia la classe
+            if (livelloSelezionato === "Easy") {
+                createSquare.classList.add('square-easy');
+            } else if (livelloSelezionato === "Medium") {
+                createSquare.classList.add('square-medium');
+            } else if (livelloSelezionato === "Hard") {
+                createSquare.classList.add('square-hard');
+            }
         }
     }
 )
+
 
 
 
